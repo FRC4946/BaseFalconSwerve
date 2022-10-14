c291fd3976d2727e12bb07cc464fb6dea90cb860
MatthewZ0823 <emzeegotanemail2@gmail.com>
2022-10-13 22:51:36 -0400

    Set all constants except offsets,CAN ids and Inverts

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,16 +16,16 @@ public final class Constants {
         public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
 
         /* Drivetrain Constants */
-        public static final double trackWidth = Units.inchesToMeters(21.73);
-        public static final double wheelBase = Units.inchesToMeters(21.73);
-        public static final double wheelDiameter = Units.inchesToMeters(3.94);
+        public static final double trackWidth = 0.58;
+        public static final double wheelBase = 0.58;
+        public static final double wheelDiameter = 0.10033;
         public static final double wheelCircumference = wheelDiameter * Math.PI;
 
         public static final double openLoopRamp = 0.25;
         public static final double closedLoopRamp = 0.0;
 
-        public static final double driveGearRatio = (6.86 / 1.0); //6.86:1
-        public static final double angleGearRatio = (12.8 / 1.0); //12.8:1
+        public static final double driveGearRatio = (6.75 / 1.0); // 6.74:1 -> Taken from SDS website
+        public static final double angleGearRatio = (150.0/7.0 / 1.0); // 150/7:1 -> Taken from SDS website
 
         public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
                 new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
@@ -45,9 +45,9 @@ public final class Constants {
         public static final boolean driveEnableCurrentLimit = true;
 
         /* Angle Motor PID Values */
-        public static final double angleKP = 0.6;
+        public static final double angleKP = 0.2;
         public static final double angleKI = 0.0;
-        public static final double angleKD = 12.0;
+        public static final double angleKD = 0.1;
         public static final double angleKF = 0.0;
 
         /* Drive Motor PID Values */
@@ -62,8 +62,18 @@ public final class Constants {
         public static final double driveKA = (0.27 / 12);
 
         /* Swerve Profiling Values */
-        public static final double maxSpeed = 4.5; //meters per second
-        public static final double maxAngularVelocity = 11.5;
+        //  The formula for calculating the theoretical maximum velocity is:
+        //   <Motor free speed RPM> / 60 * <Drive reduction> * <Wheel diameter meters> * pi
+        //  By default this value is setup for a Mk3 standard module using Falcon500s to drive.
+        //  An example of this constant for a Mk4 L2 module with NEOs to drive is:
+        //   5880.0 / 60.0 / SdsModuleConfigurations.MK4_L2.getDriveReduction() * SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI
+        public static final double maxSpeed = 6380.0 / 60.0 *
+            ((14.0 / 50.0) * (27.0 / 17.0) * (15.0 / 45.0)) * 
+            wheelDiameter * Math.PI; //meters per second
+
+        // Here we calculate the theoretical maximum angular velocity. You can also replace this with a measured amount.
+        public static final double maxAngularVelocity = maxSpeed /
+            Math.hypot(trackWidth / 2.0, wheelBase / 2.0);
 
         /* Neutral Modes */
         public static final NeutralMode angleNeutralMode = NeutralMode.Coast;
