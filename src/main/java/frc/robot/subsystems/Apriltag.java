2d771649634b3c1def0677e79c8e465fed226207
Luca Iacovelli <liaco1306@gmail.com>
2022-11-30 13:14:32 -0500

    documented vision subsystem

--- src/main/java/frc/robot/subsystems/Apriltag.java
+++ src/main/java/frc/robot/subsystems/Apriltag.java
@@ -15,36 +15,70 @@ public class Apriltag extends SubsystemBase {
 
         camera = new PhotonCamera("photonvision"); //TODO: get camera name
 
+
     }
 
+    /**
+     * 
+     * @param on
+     */
     public void setCameraFilter(boolean on) {
         camera.setDriverMode(on);
     }
 
+    /**
+     * @return true if the camera is filtered
+     */
     public boolean getCameraFilter() {
-        return camera.getDriverMode();
+        return !camera.getDriverMode();
     }
 
+    /**
+     * @return true if the camera has a target
+     */
     public boolean getHasTarget() {
         return result.hasTargets();
     }
     
+    /**
+     * @return the integer of the current targets
+     */
     public int getTargetCount() {
         return result.getTargets().size();
     }
 
+    /**
+     * Returns the x-offset of the best target in degrees
+     * 
+     * @return a double containing the x-offset
+     */
     public double getOffsetX() {
         return result.getBestTarget().getYaw();
     }
 
+    /**
+     * Returns the y-offset of the best target in degrees
+     * 
+     * @return a double containing the y-offset
+     */
     public double getOffsetY() {
         return result.getBestTarget().getPitch();
     }
 
+    /**
+     * Returns the percentage of area the best target takes up on the camera
+     * 
+     * @return a double containing the best target area
+     */
     public double getTargetArea() {
         return result.getBestTarget().getArea();
     }
 
+    /**
+     * 
+     * 
+     * @return 
+     */
     public double getDistance() {
         return 0.0; //TODO fix
     }
