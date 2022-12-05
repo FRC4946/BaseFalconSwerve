fa0370274a2f37c31a28ca61a28a461b453cfaa5
depressed-fish <tapmaster95@gmail.com>
2022-12-05 13:07:20 -0500

    Made vision actually for apriltags this time

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,97 +1,100 @@
 package frc.robot.subsystems;
 
-import org.photonvision.PhotonCamera;
-import org.photonvision.targeting.PhotonPipelineResult;
+import java.util.List;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Vision extends SubsystemBase {
+public class Vision extends SubsystemBase{
+  private final PhotonCamera camera;
+  private PhotonPipelineResult result;
 
-    PhotonCamera camera;
-    PhotonPipelineResult result;
+  //TODO: get these, also should prolly place into constants
+  //also heights in meters
+  private double cameraHeight = 0;
+  private double targetHeight = 0;
+  private double cameraPitch = 0;
 
-    public Vision() {
+  private int targetTagId = 5;
 
-        camera = new PhotonCamera("Microsoft_LifeCam_HD-3000"); //TODO: get camera name
+  private PhotonTrackedTarget target;
+  private List<PhotonTrackedTarget> targets;
 
+  public Vision() {
+    camera = new PhotonCamera("Microsoft_LifeCam_HD-3000"); //TODO: get this
+    result = camera.getLatestResult();
+    targets = result.getTargets();
+  }
 
+  public void setTargetTag(int target) {
+    targetTagId = target;
+  }
+
+  public void update() {
+    result = camera.getLatestResult();
+    targets = result.getTargets();
+    if (targets.contains(targetTagId)) {
+      target = targets.get(targets.indexOf(targetTagId));
     }
+  }
 
-    /**
-     * 
-     * @param on
-     */
-    public void setCameraFilter(boolean on) {
-        camera.setDriverMode(on);
+  public double getDistToTarget() {
+    return PhotonUtils.calculateDistanceToTargetMeters(cameraHeight, targetHeight, cameraPitch, Units.degreesToRadians(target.getPitch()));
+  }
+
+  /**
+   * 
+   * @return
+   */
+  public Translation2d getTranslationToTarget() {
+    return PhotonUtils.estimateCameraToTargetTranslation(getDistToTarget(), Rotation2d.fromDegrees(target.getYaw()));
+  }
+
+  public boolean hasTarget() {
+    return targets.contains(targetTagId);
+  }
+
+  public double getOffsetZ() {
+    return target.getBestCameraToTarget().getZ();
+  }
+
+  public double getOffsetY() {
+    return target.getBestCameraToTarget().getY();
+  }
+
+  public double getOffsetX() {
+    return target.getBestCameraToTarget().getX();
+  }
+
+  public void setCameraFilter(boolean on) {
+    camera.setDriverMode(on);
+  }
+
+  public boolean getCameraFilter() {
+    return !camera.getDriverMode();
+  } 
+
+  @Override
+  public void periodic() {
+    update();
+    SmartDashboard.putNumber("distance to target", getDistToTarget());
+    SmartDashboard.putBoolean("has target?", hasTarget());
+    SmartDashboard.putNumber("tag id", targetTagId);
+    SmartDashboard.putNumber("x offset", getOffsetX());
+    SmartDashboard.putNumber("y offset", getOffsetY());
+    SmartDashboard.putNumber("z offset", getOffsetZ());
+    double[] tagsSen = new double[targets.size()];
+    for (int i=0; i < targets.size(); i++) {
+      tagsSen[i] = (double) targets.get(i).getFiducialId();
     }
-
-    /**
-     * @return true if the camera is filtered
-     */
-    public boolean getCameraFilter() {
-        return !camera.getDriverMode();
-    }
-
-    /**
-     * @return true if the camera has a target
-     */
-    public boolean getHasTarget() {
-        return result.hasTargets();
-    }
-    
-    /**
-     * @return the integer of the current targets
-     */
-    public int getTargetCount() {
-        return result.getTargets().size();
-    }
-
-    /**
-     * Returns the x-offset of the best target in degrees
-     * 
-     * @return a double containing the x-offset
-     */
-    public double getOffsetX() {
-        return result.getBestTarget().getYaw();
-    }
-
-    /**
-     * Returns the y-offset of the best target in degrees
-     * 
-     * @return a double containing the y-offset
-     */
-    public double getOffsetY() {
-        return result.getBestTarget().getPitch();
-    }
-
-    /**
-     * Returns the percentage of area the best target takes up on the camera
-     * 
-     * @return a double containing the best target area
-     */
-    public double getTargetArea() {
-        return result.getBestTarget().getArea();
-    }
-
-    /**
-     * 
-     * 
-     * @return 
-     */
-    public double getDistance() {
-        return 0.0; //TODO fix
-    }
-
-    @Override
-    public void periodic() {
-
-        result = camera.getLatestResult();
-
-        SmartDashboard.putBoolean("Has Target", getHasTarget());
-        SmartDashboard.putNumber("Target Count", getTargetCount());
-        SmartDashboard.putNumber("Target Area", getTargetArea());
-        SmartDashboard.putNumber("Camera-X Offset", getOffsetX());
-    }
-
+    SmartDashboard.putNumberArray("current tags", tagsSen);
+  }
 }
